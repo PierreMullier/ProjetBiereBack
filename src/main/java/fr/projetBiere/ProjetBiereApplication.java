586ee06aa0464f28ae0d90cd3829563013899f6e
PierreMullier <pierre.mullier2@gmail.com>
2022-06-27 09:57:32 +0200

    My first commit

---
+++ src/main/java/fr/projetBiere/ProjetBiereApplication.java
@@ -0,0 +1,13 @@
+package fr.projetBiere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBiereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetBiereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/projetBiere/ProjetBiereApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.projetBiere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetBiereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
