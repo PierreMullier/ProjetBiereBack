c6ba6abb61684ec58cae304b2decc0d38eaef972
stagiaire <stagiaire@DESKTOP-M9ABM6P.lan>
2022-06-30 14:05:24 +0200

    Avis réparé par AvisKey

--- src/main/java/fr/projetBiere/dao/AvisRepository.java
+++ src/main/java/fr/projetBiere/dao/AvisRepository.java
@@ -1,9 +1,13 @@
 package fr.projetBiere.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.projetBiere.entities.Avis;
 
 public interface AvisRepository extends CrudRepository<Avis, Long>{
+
+
 
 }
--- src/main/java/fr/projetBiere/rest/AvisRest.java
+++ src/main/java/fr/projetBiere/rest/AvisRest.java
@@ -1,10 +1,23 @@
 package fr.projetBiere.rest;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.projetBiere.dao.AvisRepository;
+import fr.projetBiere.entities.Avis;
+import fr.projetBiere.entities.Bar;
+import fr.projetBiere.entities.User;
+
 
 @RestController 
 @CrossOrigin("*")
@@ -12,4 +25,35 @@ public class AvisRest {
 
 	@Autowired
 	AvisRepository avisRepo;
+
+	@GetMapping("/avis")
+	public List<Avis> getAllAvis(){
+		return (List<Avis>) avisRepo.findAll();
+	}
+
+	@PostMapping("/avis/save")
+	public Avis saveAvis(@RequestBody Avis av) {
+		return avisRepo.save(av);	
+	}
+
+	@DeleteMapping("avis/{id}")
+	public boolean deleteAvis(@PathVariable Long id) {
+		Optional<Avis> av = avisRepo.findById(id);
+		if(av.isEmpty()) {
+			return false;
+		}else {
+			avisRepo.deleteById(id);
+		}
+		return true;
+	}
+
+
+	// insertion d'un avis
+		@PostMapping(value = "/avis/ajout")
+		public Avis insertAvis(@RequestBody Avis av) {
+			
+		return avisRepo.save(av);
+		}
+		
+		
 }
